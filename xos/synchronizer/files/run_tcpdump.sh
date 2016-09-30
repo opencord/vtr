4efbe568bc19054df2b9bbc76b917174bda07e79
Scott Baker <smbaker@gmail.com>
2016-09-30 16:06:19 -0700

    fix issues with tcpdump truckroll
    Change-Id: I6d2d973c9f6fa71baca0f0407564ee34370d6a2e

--- xos/synchronizer/files/run_tcpdump.sh
+++ xos/synchronizer/files/run_tcpdump.sh
@@ -1,9 +1,5 @@
 #! /bin/bash
 INTERFACE=$1
-tcpdump -n -e -i $INTERFACE -c 100 &
-PID_TCPDUMP=$!
-curl http://www.xosproject.org/ &> /dev/null &
-PID_CURL=$!
-sleep 30s
-kill $PID_TCPDUMP
-kill $PIUD_CURL
+tcpdump -n -e -i $INTERFACE -c 10 &
+curl http://xosproject.org/ &> /dev/null &
+sleep 10s
