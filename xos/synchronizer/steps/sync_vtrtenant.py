f09136a6b01bcbe160cbbb975e28e6aa5d864c94
Sapan Bhatia <gwsapan@gmail.com>
2017-01-24 19:32:59 +0100

    CORD-706: Make room for new Ansible-invocation interface
    Change-Id: I9e1834f2a74444aa987f57e117043812f9f19a23

--- xos/synchronizer/steps/sync_vtrtenant.py
+++ xos/synchronizer/steps/sync_vtrtenant.py
@@ -6,7 +6,7 @@ import time
 from django.db.models import F, Q
 from xos.config import Config
 from synchronizers.base.syncstep import SyncStep
-from synchronizers.base.ansible import run_template_ssh
+from synchronizers.base.ansible_helper import run_template_ssh
 from synchronizers.base.SyncInstanceUsingAnsible import SyncInstanceUsingAnsible
 from core.models import Service, Slice, Tag
 from services.vsg.models import VSGService, VCPE_KIND
