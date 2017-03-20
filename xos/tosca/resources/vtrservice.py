ef0256fc98b9ae96c3fd57a62b663d9c503cadd0
Scott Baker <smbaker@gmail.com>
2017-03-20 10:53:17 -0700

    CORD-1057 remove VTR service reference to CoarseTenant
    Change-Id: I4d005642dc7dc1025207ef92da7c44b58a610cf6

--- xos/tosca/resources/vtrservice.py
+++ xos/tosca/resources/vtrservice.py
@@ -1,6 +1,5 @@
 from xosresource import XOSResource
 from service import XOSService
-from core.models import Service, User, CoarseTenant
 from services.vtr.models import VTRService
 
 class XOSVTRService(XOSService):
